29493ac4b23250849ca79dfbaf12c6bc231283b9
Your Nameshahrukhdigitalwolf <mohammadshahrukh.digitalwolf@gmail.com>
2025-07-19 19:04:56 +0530

    changes update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,35 +3,39 @@ const closeNavbar = document.getElementById("closeNav");
 const navbar = document.getElementById("navbar");
 const header = document.getElementById("header");
 
-openNavbar.addEventListener("click",()=>{
-    navbar.classList.remove("translate-x-[100%]")
-    navbar.classList.add("translate-x-[0%]")
-});
-closeNavbar.addEventListener("click",()=>{
-    navbar.classList.add("translate-x-[100%]")
-    navbar.classList.remove("translate-x-[0%]")
+openNavbar.addEventListener("click", () => {
+    navbar.classList.remove("translate-x-[100%]");
+    navbar.classList.add("translate-x-[0%]");
+    document.body.classList.add("overflow-hidden"); // Disable scroll
 });
 
-window.addEventListener('scroll', function (e) {
-  if (window.pageYOffset > 0) {
-    header.classList.add('bg-[#FFF]', 'shadow-lg');
-    header.classList.remove('bg-transparent');
-      } else {
-        header.classList.add('bg-transparent');
-        header.classList.remove('bg-[#FFF]','shadow-lg');
-      }
+closeNavbar.addEventListener("click", () => {
+    navbar.classList.add("translate-x-[100%]");
+    navbar.classList.remove("translate-x-[0%]");
+    document.body.classList.remove("overflow-hidden"); // Enable scroll
 });
+
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 0) {
+        header.classList.add('bg-[#FFF]', 'shadow-lg');
+        header.classList.remove('bg-transparent');
+    } else {
+        header.classList.add('bg-transparent');
+        header.classList.remove('bg-[#FFF]', 'shadow-lg');
+    }
+});
+
 document.querySelectorAll('.dropdown-button').forEach((dropdownBtn) => {
     dropdownBtn.addEventListener('click', () => {
         const dropdownMenu = dropdownBtn.nextElementSibling;
         const icon = document.querySelector(".dropdown-icon");
-        // Check if the dropdown is currently expanded
+
         if (dropdownMenu.style.maxHeight) {
-            dropdownMenu.style.maxHeight = null; // Collapse the menu
-            icon.style.rotate = '0deg'
+            dropdownMenu.style.maxHeight = null;
+            icon.style.rotate = '0deg';
         } else {
-            dropdownMenu.style.maxHeight = dropdownMenu.scrollHeight + 'px'; // Expand the menu to the full content height
-            icon.style.rotate = '180deg'
+            dropdownMenu.style.maxHeight = dropdownMenu.scrollHeight + 'px';
+            icon.style.rotate = '180deg';
         }
     });
 });
